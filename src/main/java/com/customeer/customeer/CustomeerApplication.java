c348a3e47221de130a9f744a88a60e4b96d0983f
Eakjot Hundal <eakjothundal@gmail.com>
2023-10-21 19:49:07 -0700

    initial commit

--- src/main/java/com/customeer/customeer/CustomeerApplication.java
+++ src/main/java/com/customeer/customeer/CustomeerApplication.java
@@ -1,7 +1,7 @@
 package com.customeer.customeer;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication; // Spring Boot
 
 @SpringBootApplication
 public class CustomeerApplication {
