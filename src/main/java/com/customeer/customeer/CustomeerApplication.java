50e3b249974ac0748a37fbaacdc9463f4aa3dee2
Eakjot Hundal <eakjothundal@gmail.com>
2023-10-21 19:45:52 -0700

    initial commit

---
+++ src/main/java/com/customeer/customeer/CustomeerApplication.java
@@ -0,0 +1,13 @@
+package com.customeer.customeer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomeerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomeerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/customeer/customeer/CustomeerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.customeer.customeer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomeerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
